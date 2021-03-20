6c7eda1f0f06f3b0bae4088906461c5a4d1383bf
Dipesh Bishwakarma <deepeesh471@gmail.com>
2021-03-20 07:37:39 +0545

    urls initalized

--- Bill/urls.py
+++ Bill/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import BillEdit,BillDelete,BillDetails,NewBill
 
 urlpatterns = [
-
+    path('register/', NewBill, name='add_bill'),
+    path('details/', BillDetails, name='bill_details'),
+    path('edit/<slug:slug>/', BillEdit, name='edit_bill'),
+    path('delete/<slug:slug>/', BillDelete, name='delete_bill'),
 ]
